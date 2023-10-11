a316cbc67ab88ad1cc8ce95877c27cbd488c331f
jden <jdgoh@swiftx.local>
2023-10-11 18:05:08 +0800

    created new java files

---
+++ The Dijkstra’s Algorithm/src/lab2/Edge.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class Edge {
+	int head;
+	int tail;
+	int w;	//weight
+	
+	public Edge(int head, int tail, int w) {
+		this.head = head;
+		this.tail = tail;
+		this.w = w;
+	}
+	
+}
